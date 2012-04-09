c72ffc9824f2490ff8c5111e5c61fe2f404a4f4c
Robotys <robotys@gmail.com>
2012-04-09 11:28:45 +0800

    Finished and steady!

---
+++ assets/script-copy.js
@@ -0,0 +1,197 @@
+$(function(){
+
+	//first time check kalau no hash send to board
+	if(location.hash == ""){
+		window.location = "index.html#board";
+	}
+	
+	
+	//console.log(pics.piku[0].key)
+	//randomly put awan
+	//decide how many awan (random)
+	var awan_total = 0;
+	var gunung_total = 0;
+	var awan_width = 0;
+	var awan_style = 0;
+	var awan_layer = 0;
+	var gunung_width = 0;
+	var gunung_style = 0;
+	var gunung_layer = 0;
+	var awan_x = 0;
+	var awan_xx = new Array();
+	var awan_y = 0;
+	var gunung_x = 0;
+	var gunung_xx = new Array();
+	var gunung_y = 0;
+	var ratio = [0,50,70,100];
+	
+	var pos_x = 0;
+	var pos_gunung_x = 0;
+	
+	awan_total = Math.round(Math.random()*4+1);
+	gunung_total = Math.round(Math.random()*1+1);
+	
+	//place awan for the first time
+	for(var i = 0; i<awan_total; i++){
+		awan_style = Math.round(Math.random()*5+1);
+		awan_layer = Math.round(Math.random()*2+1);
+		awan_width = Math.round(Math.random()*100+50);
+		awan_x = Math.round(Math.random()*window.innerWidth);
+		awan_y = Math.round(Math.random()*(window.innerHeight/8));
+		
+		$('.background').append("<img src='assets/elements/awan"+awan_style+".png' class='awan awan"+i+"' style='position:absolute; left:"+awan_x+"px; top:"+awan_y+"px; width:"+awan_width+"px; z-index:"+awan_layer+"'/>");
+	}
+		
+	//place gunung for the first time
+	for(var iii = 0; iii<gunung_total; iii++){
+		gunung_style = Math.round(Math.random()*1+1);
+		//gunung_layer = Math.round(Math.random()*2+1);
+		gunung_x = Math.round(Math.random()*window.innerWidth);
+		gunung_y = Math.round(Math.random()*(window.innerHeight/8));
+		
+		if(gunung_style == 1) z_index = 1;
+		else z_index = 3;
+		
+		$('.background').append("<img src='assets/elements/gunung"+gunung_style+".png' id='gunung"+iii+"' style='z-index:"+z_index+"'/>");
+		$('#gunung'+iii).css({position: 'absolute', top: '190px', left: gunung_x+'px'});
+	}
+	
+	
+	//$("#peoples img").hide();
+	//$(window.location.hash).show();
+		
+	//MANAGE ARROW CLICK
+	//set JSON of array of pictures
+	var pics = {"piku": [
+							{"key": "board", "name": "Pengarah", "top": "-20", "width": "700", "left":"-80", "youtube":"5Bertw-RRzY"},
+							{"key": "akaun", "name": "Jabatan Akaun", "top": "-20", "width": "500", "youtube":"8BTwAxDWq7c"},
+							{"key": "hr", "name": "Jabatan Sumber Manusia", "top": "10", "width": "500", "left":"140", "youtube":"0_WisCsdoQ0"},
+							{"key": "production", "name": "Jabatan Produksi", "top": "-50", "width": "600", "left": "-100", "youtube":"Lv3VSGeI3Bg"},
+							{"key": "sales", "name": "Jabatan Jualan", "top": "0", "width": "1100", "left":"-270", "youtube":"8BTwAxDWq7c"},
+							{"key": "marketing", "name": "Jabatan Pemasaran", "top": "20", "width": "1100", "left" : "-280", "youtube":"8BTwAxDWq7c"},
+							{"key": "grafik", "name": "PTS Grafik", "top": "0", "width": "700", "left": "-80", "youtube":"GvJ8QQtKOLk"},
+							{"key": "edar", "name": "PTS Edar", "top": "-20", "width": "320", "youtube":"CH5IRQPQUEo"},
+							{"key": "bookcafe", "name": "PTS Bookcafe", "top": "-70", "width": "480", "youtube":"8BTwAxDWq7c"},
+							{"key": "milenia", "name": "PTS Millenia", "top": "-30", "width": "460", "youtube":"lXrsrPbz_WM"},
+							{"key": "pro", "name": "PTS Profesionals", "top": "-30", "width": "700", "left":"-100", "youtube":"MvemhojcVMs"},
+							{"key": "one", "name": "PTS One", "top": "00", "width": "700", "left":"-80", "youtube":"XrcrgzeyCPc"},
+							{"key": "pop", "name": "PTS POP", "top": "-30", "width": "450", "youtube":"z4iZK5lQB0E"},							
+							{"key": "islamika", "name": "PTS Islamika", "top": "0", "width": "770", "left":"-90", "youtube":"8BTwAxDWq7c"},
+							{"key": "litera", "name": "PTS litera", "top": "-10", "width": "900", "left":"-200", "youtube":"bGp_KHtvPHU"},
+							{"key": "netmedia", "name": "PTS Netmedia", "top": "100", "width": "300", "youtube":"lCqUvCIk-k0"},
+							{"key": "furqan", "name": "PTS Furqan", "top": "", "width": "400", "youtube":"8BTwAxDWq7c"},						
+							{"key": "warehouse", "name": "Tim Gempur (Gudang)", "top": "100", "width": "1100", "left": "-280", "youtube":"6TPkVOlkJx4"},
+				]}
+	var binder = {
+				"board":{ "youtube":"5Bertw-RRzY", "name": "Pengarah", "top": "-20", "width": "700"},
+				"akaun":{ "youtube":"8b-BiG_q0iE", "name": "Jabatan Akaun", "top": "-20", "width": "500"},
+				"hr":{ "youtube":"0_WisCsdoQ0", "name": "Jabatan Sumber Manusia", "top": "", "width": "500"},
+				"production":{ "youtube":"Lv3VSGeI3Bg", "name": "Jabatan Produksi", "top": "-50", "width": "600"},
+				"sales":{ "youtube":"1cOekq-dZU0", "name": "Jabatan Jualan", "top": "0", "width": "1100"},
+				"marketing":{ "youtube":"1ZRKvS319XY", "name": "Jabatan Pemasaran", "top": "0", "width": "1200"},
+				"grafik":{ "youtube":"Es8m7jASemo", "name": "PTS Grafik", "top": "0", "width": "700"},
+				"edar":{ "youtube":"CH5IRQPQUEo", "name": "PTS Edar", "top": "-20", "width": "320"},
+				"bookcafe":{ "youtube":"HBDK54d6Pl4", "name": "PTS Bookcafe", "top": "-70", "width": "480"},
+				"milenia":{ "youtube":"lXrsrPbz_WM", "name": "PTS Millenia", "top": "-30", "width": "460"},
+				"pro":{ "youtube":"MvemhojcVMs", "name": "PTS Profesionals", "top": "-30", "width": "700"},
+				"one":{ "youtube":"XrcrgzeyCPc", "name": "PTS One", "top": "00", "width": "700"},
+				"pop":{ "youtube":"z4iZK5lQB0E", "name": "PTS POP", "top": "-30", "width": "450"},
+				"islamika":{ "youtube":"io-QJmYOy5o", "name": "PTS Islamika", "top": "0", "width": "770"},
+				"litera":{ "youtube":"bGp_KHtvPHU", "name": "PTS litera", "top": "-10", "width": "900"},
+				"netmedia":{ "youtube":"lCqUvCIk-k0", "name": "PTS Netmedia", "top": "100", "width": "300"},
+				"furqan":{"youtube":"", "name": "PTS Furqan", "top": "", "width": "400"},			
+				"warehouse":{ "youtube":"6TPkVOlkJx4", "name": "Tim Gempur (Gudang)", "top": "100", "width": "900"}
+				}
+	
+	
+	//$(window).resize(function(){console.log($(window).width())});
+	
+	//INSERT PICS into peoples by turutan di atas
+	//POPULATE peoples divs
+	
+	for(j=0; j<pics.piku.length; j++){
+			
+		$("#peoples").append("<img id='"+pics.piku[j].key+"' name='"+pics.piku[j].name+"' src='assets/pics/"+pics.piku[j].key+".png' style='margin-top:"+pics.piku[j].top+"px; width:"+pics.piku[j].width+"px; margin-left:0px; display: none;'/>");		
+	}
+	
+	$("#peoples").append("<div class='tanda'>test</div>");
+	
+	
+	$(window).hashchange( function(){		
+		
+		
+		$("#nxt").attr('href', "#"+($(location.hash).next().attr('id')) );		
+		$("#nxt span").html(($(location.hash).next().attr('name')) );		
+		
+		//if($(location.hash).prev().attr('id') == undefined) console.log("yeah")
+		$("#prev").attr('href', "#"+($(location.hash).prev().attr('id')) );		
+		$("#prev span").html(($(location.hash).prev().attr('name')) );		
+	
+		
+		//if first child, set prev to last child		
+		if(window.location.hash == "#"+$("#peoples img:first").attr('id')){
+			$("#prev").attr('href', "#"+($("#peoples img:last").attr('id')) );		
+			$("#prev span").html(($("#peoples img:last").attr('name')) );	
+		}
+		
+		//if last child, set next to first
+		if(window.location.hash == "#"+$("#peoples img:last").attr('id')){
+		
+			$("#nxt").attr('href', "#"+($("#peoples img:first").attr('id')) );		
+			$("#nxt span").html(($("#peoples img:first").attr('name')) );	
+		}
+		
+		$("#peoples img").hide();
+		
+		$(window.location.hash).fadeIn(500);
+		
+		$('.emb').html("<iframe width='300' height='215' src='http://www.youtube.com/embed/"+binder[location.hash.replace("#","")]['youtube']+"' frameborder='0' allowfullscreen></iframe>");
+		$('.FB').html("<div id='fb-root'></div><script src='http://connect.facebook.net/en_US/all.js#xfbml=1'></script><fb:comments href='pts.com.my/kadraya2011"+location.hash+"' num_posts='0' width='500'></fb:comments>");
+		$('.tanda').html(binder[location.hash.replace("#","")].name);
+	})
+	
+	
+	//MOUSE EFFECT
+	//get x asal semua awan;
+	
+	for(var ii=0;ii<awan_total;ii++){
+		awan_xx[ii] = $('.awan'+ii).position().left;
+		//gunung_xx[ii] = $('#gunung'+ii).position().left;
+	}
+	
+	for(var ii=0;ii<gunung_total;ii++){
+		//awan_xx[ii] = $('.awan'+ii).position().left;
+		gunung_xx[ii] = $('#gunung'+ii).position().left;
+	}
+	
+	var pos_xx_people = $("#peoples").position().left - ($("#peoples").width());
+	
+	console.log($("#peoples").position().left);
+	
+	$(document).mousemove(function(e){	
+		
+		for(var ii = 0; ii<awan_total; ii++){
+			
+			pos_x = awan_xx[ii]+Math.round((ratio[$('.awan'+ii).css('z-index')])*((e.pageX/window.innerWidth)-0.5));
+			
+			$('.awan'+ii).css('left', pos_x);
+			
+		}
+		
+		for( ii = 0; ii<gunung_total; ii++){
+			
+			pos_x = gunung_xx[ii]+Math.round((ratio[$('.awan'+ii).css('z-index')])*((e.pageX/window.innerWidth)-0.5));
+			
+			$('#gunung'+ii).css('left', pos_x);
+			
+		}
+		//$("#peoples").position().left 
+		//var pos_x_people = pos_xx_people+Math.round(250*((e.pageX/window.innerWidth)-0.5));
+		var pos_x_people = (e.pageX-$(window).width()/2)*0.3;
+		
+		$('#peoples').css('left', pos_x_people);
+		
+	}); 
+	
+	
+});
